a389b2aaf7df917c58c263bec96f528bea77e9a3
teeeya <teeeya@gmail.com>
2014-10-13 10:15:28 +0100

    Technical tests - longest ordered sequence and selenium

---
+++ src/frontEndTest/frontEnd.java
@@ -0,0 +1,67 @@
+package frontEndTest;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * @author Fathea Chowdhury
+ *
+ */
+public class frontEnd {
+
+	public static void main(String[] args) {
+		String searchString = "test";
+		String specifyLanguage = "Italiano";
+		 WebDriver driver =  new HtmlUnitDriver();
+		 //Navigate to the Wikipedia home page, http://www.wikipedia.org/.
+		 driver.get("http://www.wikipedia.org/");
+		 
+		 /**
+		 (a) Type in a string given as parameter in the search input eld.
+		 (b) Select English as the search language.
+		 (c) Click the search button.
+		 **/
+		 WebElement searchInput = driver.findElement(By.id("searchInput"));
+		 searchInput.sendKeys(searchString);
+		 
+		 Select languageDropdownList = new Select(driver.findElement(By.id("searchLanguage")));   
+		 languageDropdownList.selectByVisibleText("English");
+		 
+		 WebElement searchButton = driver.findElement(By.name("go"));
+		 searchButton.click();
+		 
+		 
+		 //WebDriverWait wait = new WebDriverWait(driver, 5);
+		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("content")));
+		 /**
+		  * Validate that the first heading of the search results page matches the search string (ignoring the case)
+		 */
+		
+		 WebElement firstHeading = driver.findElement(By.xpath("//*[@id=\"firstHeading\"]/span")); 
+		 Assert.assertEquals(firstHeading.getText().toLowerCase(),searchString.toLowerCase());
+		/** 
+		 Verify that the search results page is available in a language given as
+		 parameter.
+		 **/
+		 WebElement newLanguage = driver.findElement(By.xpath("//*[@id=\"p-lang\"]/div/ul/li[12]/a"));
+		 Assert.assertEquals(newLanguage.getText(), specifyLanguage);
+		
+		/**
+		 * Navigate to the search results page in that language.
+		 */
+		newLanguage.click();
+		
+		/**
+		Validate that the search results page in the new language includes a
+		link to the version in English.
+		**/
+		 Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"p-lang\"]/div/ul/li[7]/a")).isDisplayed());	
+	}
+
+
+
+}
---
+++ src/longestOrdered/longestordered.java
@@ -0,0 +1,46 @@
+package longestOrdered;
+/**
+ * @author Fathea Chowdhury
+ *
+ */
+public class longestordered {
+	
+	public static void main(String[] params) {
+		int[] sequence = { 0,1,2,3,4,5,6,100,2,3,3,3};
+		System.out.println(longestSubstring(sequence));
+	}
+	public static int longestSubstring(int [] sequence){
+		int longestSubstring = 0;
+		int counter = 1;
+		for (int i = 0; i < sequence.length; i++) {
+			if (i < sequence.length - 1) {
+				if (sequence[i] <= sequence[i + 1]) {
+					counter++;
+				} else {
+					if (longestSubstring < counter) {// counter is larger than
+														// current longest
+														// substring
+						longestSubstring = counter;
+						counter = 1;
+					} else { // the longest substring is larger - reset counter
+						counter = 1;
+					}
+				}
+			}
+			// Executes when end of array is reached
+			else if (i == sequence.length - 1) {
+				if (sequence[i] >= sequence[i - 1]) {// counter is larger than
+													// current longest substring
+					counter++;
+					if (longestSubstring < counter) {
+						longestSubstring = counter;
+					}
+				} else {
+					counter = 1;
+				}
+			}
+		}
+		return longestSubstring;
+	}
+
+}
